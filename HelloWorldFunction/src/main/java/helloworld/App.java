fe92f605adfa97d06ff507c15f5fd65ff607fef7
Michael McMasters <jmcmasters411@gmail.com>
2023-07-23 03:03:48 -0400

    Clean up code, create DynamoDbService

--- HelloWorldFunction/src/main/java/helloworld/App.java
+++ HelloWorldFunction/src/main/java/helloworld/App.java
@@ -24,96 +24,76 @@ import software.amazon.awssdk.services.dynamodb.model.*;
  */
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+    private DynamoDbService dynamoDbService;
+
+
+    public App() {
+        this.dynamoDbService = new DynamoDbService();
+    }
+
     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
-        String version = "7";
-        System.out.println("Version " + version);
-
         try {
-            String tableNameEnv = System.getenv("TABLE_NAME");
-            System.out.println("tableName is " + tableNameEnv);
+            String version = "8";
+            System.out.println("Version " + version);
 
-            String tableName = tableNameEnv;
-            String key = "id";
-            String keyVal = "pie7";
-            String albumTitle = "albumTitle";
-            String albumTitleValue = "albumTitleValue";
-            String awards = "awards";
-            String awardVal = "awardVal";
-            String songTitle = "songTitle";
-            String songTitleVal = "songTitleVal";
+            dynamoDbService.saveExample();
+            System.out.println("Completed saving to DynamoDb");
 
-            Region region = Region.US_EAST_1;
-            DynamoDbClient ddb = DynamoDbClient.builder()
-                    .region(region)
-                    .build();
-
-            putItemInTable(ddb, tableName, key, keyVal, albumTitle, albumTitleValue, awards, awardVal, songTitle, songTitleVal);
-            System.out.println("Done!");
-            ddb.close();
+            String body = String.format("{ \"message\": \"hello world\", \"version\": \"%s\" }", version);
+            return generateResponse(true, body);
         } catch (Exception ex) {
-            System.out.println("Exception writing to DynamoDb. Exception is ...");
+            System.out.println("Exception in handler. Exception is...");
             System.out.println(ex);
+            return generateResponse(false, "{}");
         }
+    }
 
+    private APIGatewayProxyResponseEvent generateResponse(boolean successful, String body) {
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("X-Custom-Header", "application/json");
 
-        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                .withHeaders(headers);
-        try {
-            final String pageContents = this.getPageContents("https://checkip.amazonaws.com");
-            String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\", \"version\": \"%s\" }", pageContents, version);
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(headers);
 
+        if (successful) {
             return response
                     .withStatusCode(200)
-                    .withBody(output);
-        } catch (IOException e) {
+                    .withBody(body);
+        } else {
             return response
-                    .withBody("{}")
+                    .withBody(body)
                     .withStatusCode(500);
         }
     }
 
-    public static void putItemInTable(DynamoDbClient ddb,
-                                      String tableName,
-                                      String key,
-                                      String keyVal,
-                                      String albumTitle,
-                                      String albumTitleValue,
-                                      String awards,
-                                      String awardVal,
-                                      String songTitle,
-                                      String songTitleVal){
-
-        HashMap<String,AttributeValue> itemValues = new HashMap<>();
-        itemValues.put(key, AttributeValue.builder().s(keyVal).build());
-        itemValues.put(songTitle, AttributeValue.builder().s(songTitleVal).build());
-        itemValues.put(albumTitle, AttributeValue.builder().s(albumTitleValue).build());
-        itemValues.put(awards, AttributeValue.builder().s(awardVal).build());
-
-        PutItemRequest request = PutItemRequest.builder()
-                .tableName(tableName)
-                .item(itemValues)
-                .build();
-
-        try {
-            PutItemResponse response = ddb.putItem(request);
-            System.out.println(tableName +" was successfully updated. The request id is "+response.responseMetadata().requestId());
-        } catch (ResourceNotFoundException e) {
-            System.err.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", tableName);
-            System.err.println("Be sure that it exists and that you've typed its name correctly!");
-            System.exit(1);
-        } catch (DynamoDbException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    private String getPageContents(String address) throws IOException{
-        URL url = new URL(address);
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-    }
+//    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+//        String version = "7";
+//        System.out.println("Version " + version);
+//
+//        try {
+//            dynamoDbService.saveExample();
+//            System.out.println("Saved successfully!");
+//        } catch (Exception ex) {
+//            System.out.println("Exception writing to DynamoDb. Exception is ...");
+//            System.out.println(ex);
+//        }
+//
+//        Map<String, String> headers = new HashMap<>();
+//        headers.put("Content-Type", "application/json");
+//        headers.put("X-Custom-Header", "application/json");
+//
+//        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+//                .withHeaders(headers);
+//        try {
+//            String output = String.format("{ \"message\": \"hello world\", \"version\": \"%s\" }", version);
+//
+//            return response
+//                    .withStatusCode(200)
+//                    .withBody(output);
+//        } catch (Exception e) {
+//            return response
+//                    .withBody("{}")
+//                    .withStatusCode(500);
+//        }
+//    }
 }
